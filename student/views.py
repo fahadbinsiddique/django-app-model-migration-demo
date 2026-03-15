311863b9ba8a6d8bc87aa6e8a0df87f93f06079d
Fahad Bin Siddique <fahad.bin.siddique345@gmail.com>
2026-03-15 23:59:27 +0600

    add modal

--- student/views.py
+++ student/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from student.models import *
-# from student.template import *
-
-# Create your views here.
 
 def Read_Data(request):
+    data=Student_Modal.objects.all()
+    Data_Pack={
+        "item": data
+    }
+    return render(request,"display.html",Data_Pack)
+
+def Employee_Data(request):
     data=Student_Modal.objects.all()
     Data_Pack={
         "item": data
